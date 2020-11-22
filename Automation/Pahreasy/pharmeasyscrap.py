95e0488773c81067a3c83b4233805b05f5c749f3
vicky <srivicky2000@gmail.com>
2020-11-22 12:31:01 +0530

    PahrEasy Scraped

--- Automation/Pahreasy/pharmeasyscrap.py
+++ Automation/Pahreasy/pharmeasyscrap.py
@@ -1,7 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
-pharma = requests.get('https://pharmeasy.in/online-medicine-order?src=header').text
+pharma = requests.get('https://pharmeasy.in/online-medicine-order/dolo-650mg-tablet-44139').text
 soup = BeautifulSoup(pharma,'lxml')
 
-print(soup.find_all('div',class_="_3cTdj"))
+Tab = soup.find_all('h1',class_="ooufh")
+TabCompany = soup.find_all('div',class_="_3JVGI")
+TabQty = soup.find_all('div',class_="_36aef")
+TabPrice = soup.find_all('div',class_="_1_yM9")
+TabRealPrice = soup.find_all('div',class_="_3FUtb")
+
+TabOffer = soup.find_all('div',class_="_306Fp")
+MedDescription = soup.find_all('div',class_="_1ZIK6")
+
+
+print(Tab[0].text)
+print(TabCompany[0].text)
+print(TabPrice[0].text)
+print(TabRealPrice[0].text)
+print(TabOffer[0].text)
+print("Medical Description ",MedDescription[0].text)
+
+
+
